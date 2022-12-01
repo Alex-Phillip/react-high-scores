5cd4706a3f08e0afb0a505f060b33462bf0cf480
Alex Phillip <alexjoelphillip@gmail.com>
2022-12-01 12:50:04 +0000

    completed levels 1, 2 + 3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import allCountryScores from "./scores";
 import HighScoreTable from "./HighScoreTable";
-import SinglePlayerScore from "./SinglePlayerScore";
+// import SinglePlayerScore from "./SinglePlayerScore";
 
 allCountryScores.sort((a, b) => {
   return a.name < b.name ? -1 : 1;
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <HighScoreTable allCountryScores={allCountryScores} />
-      <SinglePlayerScore allCountryScores={allCountryScores} />
+      {/* <SinglePlayerScore allCountryScores={allCountryScores} /> */}
     </div>
   );
 }
--- src/HighScoreTable.jsx
+++ src/HighScoreTable.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 function HighScoreTable(props) {
   
+    const sortingFunction = (a, b) => {
+        // if (a.s < b.s) {
+        //     return -1
+        // } else if (a.s > b.s) {
+        //     return 1
+        // } else {
+        //     return 0
+        // }
+        return (a.s < b.s) ? 1 : (a.s > b.s) ? -1 : 0
+    }
+
   return (
     <div className='scoresContainer'>
         <h1>High Scores per Country</h1>
@@ -11,7 +22,7 @@ function HighScoreTable(props) {
                         <h2>
                             {`HIGH SCORES: ${score.name}`}
                         </h2>
-                {score.scores.map((x) => {
+                {score.scores.sort(sortingFunction).map((x) => {
                     return (
                         <div className='scoresData'>
                         <p>{`${x.n} ${x.s}`}</p>
